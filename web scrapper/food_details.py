eff5dc5fa88394aee55d0cf6145c6149892ffb4d
rzanchetta01 <rzanchetta01@gmail.com>
2022-05-14 21:49:43 -0300

    finished webscrapper

--- web scrapper/food_details.py
+++ web scrapper/food_details.py
@@ -2,15 +2,18 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import json
 
 # path to all links of food
 # each food will be created a new csv of that data
-folder_path = "/media/rodrigozanchetta/Trabalhos/UAM/DIMAS_MOBILE_APP/food-app/web scrapper/links/links.txt"
+folder_path = "T:\\UAM\\DIMAS_MOBILE_APP\\food-app\\web scrapper\links\\links.txt"
 
-all_collumns = []
+
 
 # set collumns
 for line in open(folder_path, 'r').readlines():
+
+
 
         table_header = []
         table_header.append("codigo")
@@ -24,31 +27,40 @@ for line in open(folder_path, 'r').readlines():
         #get page html
         soup = BeautifulSoup(html_file, "html.parser")
 
-        # get food id
+       # get food name/descriptio and id
         food_title = soup.find('h5', id="overview")
         food_title = food_title.text.strip()
         food_title = food_title.split(": ")
+        
+        food_name = food_title[2].split("<<")
         food_code = food_title[1]
         food_code = food_code.replace("Descrição", " ")
 
+
+        # separete by language
+        food_name_pt = food_name[0]
+        food_name_en = ""
+        if len(food_name) > 1:
+            food_name_en = food_name[1].replace(">", "")
+
+
+
+        #t head
         header = soup.find_all("table")[0].find("tr")
         for items in header:
             try:
                 table_header.append(items.get_text())
-                if not all_collumns.__contains__(items.get_text()):
-                    all_collumns.append(items.get_text())
             except:
                 continue
 
 
-        # get food data
+        #t body
         body = soup.find_all("table")[0].find_all("tr")[1:]
 
         for element in body:
             sub_data = []
             sub_data.append(food_code)
             # get items of each row
-            count = 1
             for sub_element in element:
                 try:
                         sub_data.append(sub_element.get_text())
@@ -58,9 +70,24 @@ for line in open(folder_path, 'r').readlines():
             else :
                 table_data.append(sub_data)
 
-        # create csv
+        
+        # export data
         dataframe = pd.DataFrame(data = table_data, columns = table_header)
-        #dataframe.to_csv("/media/rodrigozanchetta/Trabalhos/UAM/DIMAS_MOBILE_APP/food-app/web scrapper/links/foods-details"+food_code+".csv",  sep=";")
 
-columns_df = pd.DataFrame(data=all_collumns)
-columns_df.to_json("/media/rodrigozanchetta/Trabalhos/UAM/DIMAS_MOBILE_APP/food-app/web scrapper/links/foods-all-columns.csv")
+        info_data = [{"name-ptbr": food_name_pt},{"name-eng": food_name_en},{"codigo": food_code}]
+        #info_data = {'carl': 33}
+        
+
+        json_path= "T:\\UAM\\DIMAS_MOBILE_APP\\food-app\\web scrapper\\links\\foods\\food_"+food_code+".json"
+        dataframe.to_json(json_path)
+
+
+        with open(json_path, "r") as file:
+            data = json.load(file)
+       
+        data["Info_Data"] = info_data
+        
+        with open(json_path, "w") as file:
+            json.dump(data, file)
+
+
